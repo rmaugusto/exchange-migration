474f0de3ba7223e364e3bd566d741c6b5f05412d
Ricardo M. Augusto <rmaugusto@gmail.com>
2024-01-30 18:14:10 -0300

    fix tradução

--- exchange-migration/emails.py
+++ exchange-migration/emails.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from exchangelib import FaultTolerance, Configuration
+from exchangelib import FaultTolerance, Configuration, FolderCollection
 from exchangelib import IMPERSONATION, Account, CalendarItem, ExtendedProperty, Folder, Message, OAuth2Credentials
 from exchangelib.items import (
     Message,
@@ -9,21 +9,131 @@ from exchangelib.items import (
 from exchangelib import Q
 from thread import ThreadPool
 import os
-
+from exchangelib.properties import DistinguishedFolderId
 
 #logging.basicConfig(level=logging.DEBUG, handlers=[PrettyXmlHandler()])
 
-FOLDERS_IGNORE = ['Sync Issues', 'Junk Email', 'Deleted Items', 'GraphFilesAndWorkingSetSearchFolder',
-'People I Know',
-'RelevantContacts',
-'SharedFilesSearchFolder',
-'Sharing',
-'SpoolsPresentSharedItemsSearchFolder',
-'SpoolsSearchFolder',
-'UserCuratedContacts','My Contacts',
-'AllContacts',
-'AllContactsExtended',
-'AllPersonMetadata',]
+FOLDERS_IGNORE = [
+    'Sync Issues', 
+    'Junk Email', 
+    'Deleted Items', 
+    'GraphFilesAndWorkingSetSearchFolder',
+    'People I Know',
+    'RelevantContacts',
+    'SharedFilesSearchFolder',
+    'Favorites',
+    'Sharing',
+    'SpoolsPresentSharedItemsSearchFolder',
+    'SpoolsSearchFolder',
+    'UserCuratedContacts','My Contacts',
+    'AllContacts',
+    'AllContactsExtended',
+    'AllPersonMetadata',
+    'Favoritos',
+    'Início do Repositório de Informações',
+    'Meus Contatos',
+    'PeopleConnect',
+    'Recoverable Items',
+    'Finder',
+    'To-Do Search',
+    'System',
+    'AllCategorizedItems',
+    'AllContacts',
+    'AllContactsExtended',
+    'AllItems',
+    'AllPersonMetadata',
+    'AllTodoTasks',
+    'ApplicationDataRoot',
+    'BrokerSubscriptions',
+    'BulkActions',
+    'Calendar Version Store',
+    'CalendarItemSnapshots',
+    'CalendarSearchCache',
+    'CalendarSharingCacheCollection',
+    'Common Views',
+    'ComplianceMetadata',
+    'Connectors',
+    'CrawlerData',
+    'DefaultFoldersChangeHistory',
+    'Deferred Action',
+    'DlpPolicyEvaluation',
+    'Document Centric Conversations',
+    'ExchangeODataSyncData',
+    'ExchangeSyncData',
+    'FileCollectionCache',
+    'Folder Memberships',
+    'Freebusy Data',
+    'FreeBusyLocalCache',
+    'GraphFilesAndWorkingSetSearchFolder',
+    'GraphStore',
+    'Inference',
+    'Lembretes',
+    'Location',
+    'MailboxAssociations',
+    'MeetingSapces',
+    'MergedViewFolderCollection',
+    'MessageIngestion',
+    'MyAnalytics-ActionLog',
+    'MyAnalytics-UnreadFromIRankerFolder',
+    'MyAnalytics-UnreadFromVIPFolder',
+    'O365 Suite Notifications',
+    'O365 Suite Storage',
+    'OneDriveRoot',
+    'OneNotePagePreviews',
+    'Orion Notes',
+    'OutlookExtensions',
+    'PACE',
+    'ParkedMessages',
+    'Pass-Through Search Results',
+    'PdpProfile',
+    'PdpProfileV2',
+    'People I Know',
+    'PeopleInsights',
+    'PeoplePublicData',
+    'QuarantinedEmail',
+    'RecoveryPoints',
+    'RelevantContacts',
+    'Schedule',
+    'SearchFoldersView',
+    'ShadowItems',
+    'ShardRelevancyFolder',
+    'SharedFilesSearchFolder',
+    'SharePointNotifications',
+    'Sharing',
+    'Shortcuts',
+    'ShortNotes',
+    'SkypeSpacesData',
+    'SmsAndChatsSync',
+    'SpamReports',
+    'Spooler Queue',
+    'SpoolsPresentSharedItemsSearchFolder',
+    'SpoolsSearchFolder',
+    'Subscriptions',
+    'SubstrateFiles',
+    'SuggestedUserGroupAssociations',
+    'SwssItems',
+    'TeamChatHistory',
+    'TeamsMessagesData',
+    'TemporarySaves',
+    'UserCuratedContacts',
+    'UserSocialActivityNotifications',
+    'Archive',
+    'Views',
+    'XrmActivityClientInstrumentation',
+    'XrmActivityServerInstrumentation',
+    'XrmActivityStream',
+    'XrmActivityStreamSearch',
+    'XrmCompanySearch',
+    'XrmDealSearch',
+    'XrmDeletedItems',
+    'XrmInsights',
+    'XrmProjects',
+    'Itens Excluídos',
+    'Lixo Eletrônico',
+    'Problemas de Sincronização',
+    'XrmSearch',
+    'YammerData'
+]
 
 def getLogger(name):
     name = name.replace('.', '_').replace('@', '_')
@@ -74,18 +184,34 @@ class ExchangeFolderMigrator:
         #print("Criado diretório: ", new_folder.absolute)
         return new_folder
 
+    def equals_translated(self, folder1, folder2):
+        if (folder1.lower() == 'inbox' and folder2.lower() == 'caixa de entrada') or \
+            (folder1.lower() == 'caixa de entrada' and folder2.lower() == 'inbox'):
+            return True
+        
+        if (folder1.lower() == 'outbox' and folder2.lower() == 'caixa de saída') or \
+            (folder1.lower() == 'caixa de saída' and folder2.lower() == 'outbox'):
+            return True
+        
+        if (folder1.lower() == 'sent items' and folder2.lower() == 'itens enviados') or \
+            (folder1.lower() == 'itens enviados' and folder2.lower() == 'sent items'):
+            return True
+
+        return folder1 == folder2
+
     def traverse_and_create(self, folder, parent_dest_folder):
-        if folder.name != parent_dest_folder.name:  # Evita duplicar root
-            new_dest_folder = self.create_or_get_folder(folder.name, parent_dest_folder)
-        else:
+        if self.equals_translated(folder.name, parent_dest_folder.name):
             new_dest_folder = parent_dest_folder
+        else:
+            new_dest_folder = self.create_or_get_folder(folder.name, parent_dest_folder)
 
-        self.map_folders[folder.id] = FolderMatch(origin=folder, dest=new_dest_folder)
+        if folder.id not in self.map_folders:
+            self.map_folders[folder.id] = FolderMatch(origin=folder, dest=new_dest_folder)
 
-        for subfolder in folder.children:
-            #Diretório de emails
-            if (subfolder.folder_class == 'IPF.Note' or subfolder.name == 'Top of Information Store' or subfolder.name == 'Início do Repositório de Informações') and subfolder.name not in FOLDERS_IGNORE:
-                self.traverse_and_create(subfolder, new_dest_folder)
+            for subfolder in folder.children:
+                #Diretório de emails
+                if (subfolder.folder_class == 'IPF.Note' or subfolder.name == 'Top of Information Store' or subfolder.name == 'Início do Repositório de Informações') and subfolder.name not in FOLDERS_IGNORE:
+                    self.traverse_and_create(subfolder, new_dest_folder)
 
     def add_contacts(self, acc_orig, acc_dest):
         folder_ori = acc_orig.contacts
@@ -96,6 +222,47 @@ class ExchangeFolderMigrator:
         folder_ori = acc_orig.calendar
         folder_dest = acc_dest.calendar
         self.map_folders[folder_ori.id] = FolderMatch(origin=folder_ori, dest=folder_dest)
+
+    def add_messages(self, acc_orig, acc_dest):
+        self.map_folders[acc_orig.inbox.id] = FolderMatch(origin=acc_orig.inbox, dest=acc_dest.inbox)
+        self.map_folders[acc_orig.outbox.id] = FolderMatch(origin=acc_orig.outbox, dest=acc_dest.outbox)
+        self.map_folders[acc_orig.sent.id] = FolderMatch(origin=acc_orig.sent, dest=acc_dest.sent)
+        self.map_folders[acc_orig.drafts.id] = FolderMatch(origin=acc_orig.drafts, dest=acc_dest.drafts)
+
+    def add_first_level(self, acc_orig, acc_dest):
+
+        root_folder_orig = None
+        root_folder_dest = None
+
+        try:
+            root_folder_orig = acc_orig.root / 'Início do Repositório de Informações'
+        except:
+            pass
+
+        try:
+            if not root_folder_orig:
+                root_folder_orig = acc_orig.root / 'Top of Information Store'
+        except:
+            pass
+
+        try:
+            root_folder_dest = acc_dest.root / 'Início do Repositório de Informações'
+        except:
+            pass
+
+        try:
+            if not root_folder_dest:
+                root_folder_dest = acc_dest.root / 'Top of Information Store'
+        except:
+            pass
+
+
+        for subfolder in root_folder_orig.children:
+
+            if subfolder.id not in self.map_folders:
+                if (subfolder.folder_class == 'IPF.Note') and subfolder.name not in FOLDERS_IGNORE:
+                    self.traverse_and_create(subfolder, root_folder_dest)
+
 
 class EmailMigrator:
     def __init__(self):
@@ -135,10 +302,17 @@ class EmailMigrator:
         acc_dest = Account(dest_email, credentials=credentials_dest, autodiscover=True,  access_type=IMPERSONATION, config=config_dest)
         
         self.folder_migrator = ExchangeFolderMigrator()
+
+        self.folder_migrator.add_messages(acc_orig, acc_dest)
         self.folder_migrator.add_contacts(acc_orig, acc_dest)
         self.folder_migrator.add_calendars(acc_orig, acc_dest)
 
-        self.folder_migrator.traverse_and_create(acc_orig.root, acc_dest.root)
+        #Cria sub dos diretorios conhecidos
+        self.folder_migrator.traverse_and_create(acc_orig.inbox, acc_dest.inbox)
+        self.folder_migrator.traverse_and_create(acc_orig.outbox, acc_dest.outbox)
+        self.folder_migrator.traverse_and_create(acc_orig.sent, acc_dest.sent)
+
+        self.folder_migrator.add_first_level(acc_orig, acc_dest)
 
         initial_time = time.time()
         self.copy_items(acc_orig, acc_orig.root, acc_dest)
@@ -146,11 +320,11 @@ class EmailMigrator:
         hours = int(total_time // 3600)
         minutes = int((total_time % 3600) // 60)
         seconds = int(total_time % 60)
+        
         # Formatando o tempo no formato hh:mm:ss
         self.logger.info(f"Finalizado em {hours:10d}:{minutes:02d}:{seconds:02d}") 
     
         self.tp.close()
-
 
 
     def copy_items(self, acc_orig, folder_ori, acc_dest):
